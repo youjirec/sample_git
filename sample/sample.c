9d6c911def2e61f42184dd5e167e3b48b191a289
ponta <youji_chaos@yahoo.co.jp>
2014-08-16 11:41:00 +0900

    tmp_functionを追加

--- sample/sample.c
+++ sample/sample.c
@@ -1,9 +1,16 @@
 #include "stdio.h"
 
+void tmp_function()
+{
+	printf("Tmp task was happened\n");
+}
+
 int main()
 {
 	printf("Hello, git2\n");
 	printf("Hello, git again!\n");
+
+	tmp_function();
 
 	return 0;
 }
