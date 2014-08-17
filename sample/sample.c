f30486fc77f06c1d4d1d793bbd1efa80ca452905
ponta <youji_chaos@yahoo.co.jp>
2014-08-17 11:27:22 +0900

    new branch

--- sample/sample.c
+++ sample/sample.c
@@ -1,16 +1,6 @@
 #include "stdio.h"
 
-void tmp_function()
-{
-	printf("Tmp task was happened\n");
-}
-
 int main()
 {
-	printf("Hello, git2\n");
-	printf("Hello, git again!\n");
-
-	tmp_function();
-
-	return 0;
+	printf("Hello, git\n");
 }
